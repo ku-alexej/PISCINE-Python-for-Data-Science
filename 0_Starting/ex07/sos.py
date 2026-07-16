8d3b4f95e6624f2c0c1f8d2ce041f439fa652a24
ku-alexej <ku.alexej@gmail.com>
2026-07-16 12:52:36 +0200

    feat: ex07

---
+++ 0_Starting/ex07/sos.py
@@ -0,0 +1,67 @@
+import sys
+
+
+def main():
+    '''
+        The program takes a string as an argument and encodes it into Morse Code
+    '''
+
+    NESTED_MORSE = {
+        ' ': '/ ',
+        'A': '.- ',
+        'B': '-... ',
+        'C': '-.-. ',
+        'D': '-.. ',
+        'E': '. ',
+        'F': '..-. ',
+        'G': '--. ',
+        'H': '.... ',
+        'I': '.. ',
+        'J': '.--- ',
+        'K': '-.- ',
+        'L': '.-.. ',
+        'M': '-- ',
+        'N': '-. ',
+        'O': '--- ',
+        'P': '.--. ',
+        'Q': '--.- ',
+        'R': '.-. ',
+        'S': '... ',
+        'T': '- ',
+        'U': '..- ',
+        'V': '...- ',
+        'W': '.-- ',
+        'X': '-..- ',
+        'Y': '-.-- ',
+        'Z': '--.. ',
+        '0': '----- ',
+        '1': '.---- ',
+        '2': '..--- ',
+        '3': '...-- ',
+        '4': '....- ',
+        '5': '..... ',
+        '6': '-.... ',
+        '7': '--... ',
+        '8': '---.. ',
+        '9': '----. ',
+    }
+
+    try:
+        assert len(sys.argv) == 2
+
+        message = sys.argv[1]
+
+        assert type(message) is str
+
+        message = message.upper()
+
+        assert all(c in NESTED_MORSE for c in message)
+
+        print("".join(NESTED_MORSE[c] for c in message).rstrip())
+
+    except AssertionError:
+        print("AssertionError: the arguments are bad")
+
+
+if __name__ == "__main__":
+    main()
