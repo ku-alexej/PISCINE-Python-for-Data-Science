bc1e702a49829b2872105602e9ea1e94b98d6135
ku-alexej <ku.alexej@gmail.com>
2026-07-16 08:26:09 +0200

    feat: ex00

--- 0_Starting/ex00/Hello.py
+++ 0_Starting/ex00/Hello.py
@@ -3,6 +3,16 @@ ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello": "titi!"}
 
+ft_list[1] = 'World!'
+
+ft_tuple = list(ft_tuple)
+ft_tuple[1] = 'France!'
+ft_tuple = tuple(ft_tuple)
+
+ft_set.remove('tutu!')
+ft_set.add('Paris!')
+
+ft_dict["Hello"] = '42Paris!'
 
 print(ft_list)
 print(ft_tuple)
