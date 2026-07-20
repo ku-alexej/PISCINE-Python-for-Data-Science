035846f99a14d6f4ecd022d0f6743037e1df703f
ku-alexej <ku.alexej@gmail.com>
2026-07-21 00:41:05 +0200

    feat: 3_ex03

--- 3_OOP/ex03/ft_calculator.py
+++ 3_OOP/ex03/ft_calculator.py
@@ -1,18 +1,26 @@
 class calculator:
-    # your code here
+    """A class that represents a simple calculator."""
 
+    def __init__(self, lst: list) -> None:
+        if not isinstance(lst, list):
+            raise TypeError("Input must be a list.")
+        self.vector = [float(i) for i in lst]
 
-def __add__(self, object) -> None:
-    # your code here
+    def __add__(self, object) -> None:
+        self.vector = [x + object for x in self.vector]
+        print(self.vector)
 
+    def __mul__(self, object) -> None:
+        self.vector = [x * object for x in self.vector]
+        print(self.vector)
 
-def __mul__(self, object) -> None:
-    # your code here
+    def __sub__(self, object) -> None:
+        self.vector = [x - object for x in self.vector]
+        print(self.vector)
 
-
-def __sub__(self, object) -> None:
-    # your code here
-
-
-def __truediv__(self, object) -> None:
-    # your code here
+    def __truediv__(self, object) -> None:
+        try:
+            self.vector = [x / object for x in self.vector]
+            print(self.vector)
+        except ZeroDivisionError as e:
+            raise ZeroDivisionError("Division by zero is not allowed.") from e
