b9b1992a73abff378bb323110ad864622ff6b44f
ku-alexej <ku.alexej@gmail.com>
2026-07-21 00:51:01 +0200

    feat: 3_ex04

--- 3_OOP/ex04/ft_calculator.py
+++ 3_OOP/ex04/ft_calculator.py
@@ -1,17 +1,20 @@
 class calculator:
-    # your code here
-    # decorator
+    """Classcalculator for 2 vectors: dot product, addition, subtraction"""
 
+    @staticmethod
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        """Dot product of two vectors."""
+        result = sum(x * y for x, y in zip(V1, V2))
+        print(f"Dot product is: {result}")
 
-def dotproduct(V1: list[float], V2: list[float]) -> None:
-    # your code here
-    # decorator
+    @staticmethod
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        """Addition of two vectors."""
+        result = [float(x + y) for x, y in zip(V1, V2)]
+        print(f"Add Vector is : {result}")
 
-
-def add_vec(V1: list[float], V2: list[float]) -> None:
-    # your code here
-    # decorator
-
-
-def sous_vec(V1: list[float], V2: list[float]) -> None:
-    # your code here
+    @staticmethod
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        """Subtraction of two vectors."""
+        result = [float(x - y) for x, y in zip(V1, V2)]
+        print(f"Sous Vector is: {result}")
