e59cef47ab1543a3fd872bec7738e1077bd1201b
ku-alexej <ku.alexej@gmail.com>
2026-07-22 20:50:20 +0200

    feat: ex03

--- 4_DOD/ex03/new_student.py
+++ 4_DOD/ex03/new_student.py
@@ -4,9 +4,22 @@ from dataclasses import dataclass, field
 
 
 def generate_id() -> str:
+    '''Generates a random string of 15 lowercase letters.'''
     return "".join(random.choices(string.ascii_lowercase, k=15))
 
 
 @dataclass
 class Student:
-    # your code here
+    '''Data class representing a studentwith attributes for
+    name, surname, active status, login, and a unique ID.'''
+
+    name: str
+    surname: str
+    active: bool = field(default=True)
+    # active: bool = field(init=False, default=True)
+    login: str = field(init=False)
+    id: str = field(init=False, default_factory=generate_id)
+
+    def __post_init__(self):
+        '''Validates attributes and generates the login.'''
+        self.login = f"{self.name[0]}{self.surname}"
--- 4_DOD/ex03/tester.py
+++ 4_DOD/ex03/tester.py
@@ -1,4 +1,38 @@
 from new_student import Student
 
-student = Student(name="Edward", surname="agle")
-print(student)
+try:
+    student = Student(name="Edward", surname="agle")
+    print(student)
+except Exception as e:
+    print(f"Error: {e}")
+
+try:
+    student = Student(name="Edward", surname="agle", active=False)
+    print(student)
+except Exception as e:
+    print(f"Error: {e}")
+
+try:
+    student = Student(name="Edward")
+    print(student)
+except Exception as e:
+    print(f"Error: {e}")
+
+
+try:
+    student = Student(name="Edward", surname="agle", id="custom_id_123")
+    print(student)
+except Exception as e:
+    print(f"Error: {e}")
+
+try:
+    student = Student(name="Edward", surname="agle", login="custom_login")
+    print(student)
+except Exception as e:
+    print(f"Error: {e}")
+
+try:
+    student = Student(name="Edward", surname="agle", wrong_param="unexpected")
+    print(student)
+except Exception as e:
+    print(f"Error: {e}")
